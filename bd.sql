61aba286ebe25e4ee02fd93a18bd658f8614d01a
EnzoLu1337 <61724474+EnzoLu1337@users.noreply.github.com>
2024-10-24 17:23:51 +0400

    Скрипт для создания таблиц в БД PSQL

---
+++ bd.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    completed BOOLEAN DEFAULT FALSE,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    end_date DATE
+);
