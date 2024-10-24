7bedfdbcc2ec3ae9e0bd35ea2c81d9ff172204e3
EnzoLu1337 <61724474+EnzoLu1337@users.noreply.github.com>
2024-10-24 14:35:54 +0400

    Добавление авторизации пользователя

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const pool = require('./database');
 
@@ -34,5 +35,35 @@ app.post('/api/register', async (req, res) => {
       console.error(err.message);
       res.status(400).send('Error registering user');
     }
+});
+
+// Авторизация пользователя
+app.post('/api/login', async (req, res) => {
+    const { username, password } = req.body;
+  
+    try {
+      const user = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+  
+      if (user.rows.length === 0) {
+        return res.status(401).send('User not found');
+      }
+  
+      const validPassword = await bcrypt.compare(password, user.rows[0].password);
+  
+      if (!validPassword) {
+        return res.status(401).send('Invalid credentials');
+      }
+  
+      const token = jwt.sign({ id: user.rows[0].id }, 'secret_key', { expiresIn: '1h' });
+      res.json({ token });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   });
 
+// Запуск сервера
+app.listen(5000, () => {
+    console.log('Server is running on http://127.0.0.1:5000');
+  });
+  
